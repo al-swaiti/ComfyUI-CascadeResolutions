7d42269f2e84678555fb6bb17ad06139322b3213
al-swaiti <abedalswaity7@gmail.com>
2024-02-25 22:52:52 +0300

    Updated CascadeResolutions to display size with name

--- cascadeResolutions.py
+++ cascadeResolutions.py
@@ -1,5 +1,6 @@
 import os
 import json
+
 class CascadeResolutions:
     def __init__(self):
         pass
@@ -7,7 +8,11 @@ class CascadeResolutions:
     @classmethod
     def INPUT_TYPES(s):
         s.size_sizes, s.size_dict = read_sizes()
-        return {'required': {'size_selected': (s.size_sizes,)}}
+        return {'required': {
+                    'size_selected': (s.size_sizes,), 
+                    'multiply_factor': ("INT", "FLOAT")
+                }
+        }
 
     RETURN_TYPES = ("INT","INT")
     RETURN_NAMES = ("width","height")
@@ -15,27 +20,26 @@ class CascadeResolutions:
     OUTPUT_NODE = True
     CATEGORY = "Resolution"
 
-    def return_res(self, size_selected):
-        width = self.size_dict[size_selected]["width"]
-        height = self.size_dict[size_selected]["height"]
-        return (width,height)
-        pass
+    def return_res(self, size_selected, multiply_factor):
+        # Extract just the key part for lookup
+        key = size_selected.split(" (")[0]
+        width = self.size_dict[key]["width"] * multiply_factor
+        height = self.size_dict[key]["height"] * multiply_factor
+        return (width, height)
 
-# A dictionary that contains all nodes you want to export with their names
-# NOTE: names should be globally unique
 NODE_CLASS_MAPPINGS = {
     "CascadeResolutions": CascadeResolutions
 }
 
-# A dictionary that contains the friendly/humanly readable titles for the nodes
 NODE_DISPLAY_NAME_MAPPINGS = {
     "CascadeResolutions": "Cascade Resolutions"
 }
+
 def read_sizes():
     p = os.path.dirname(os.path.realpath(__file__))
     file_path = os.path.join(p, 'sizes.json')
     with open(file_path, 'r') as file:
         data = json.load(file)
-    size_sizes = list(data['sizes'].keys())
-    size_dict = data['sizes']
+    size_sizes = [f"{key} ({value['name']})" for key, value in data['sizes'].items()]
+    size_dict = {key: value for key, value in data['sizes'].items()}
     return size_sizes, size_dict
