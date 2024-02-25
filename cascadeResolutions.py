11a9fa61e594b2a2dd82274124d6c4aaa9e809d5
al-swaiti <abedalswaity7@gmail.com>
2024-02-26 02:00:55 +0300

    Updated CascadeResolutions to display size with name edited

--- cascadeResolutions.py
+++ cascadeResolutions.py
@@ -6,26 +6,28 @@ class CascadeResolutions:
         pass
     
     @classmethod
-    def INPUT_TYPES(s):
-        s.size_sizes, s.size_dict = read_sizes()
-        return {'required': {
-                    'size_selected': (s.size_sizes,), 
-                    'multiply_factor': ("INT", "FLOAT")
-                }
+    def INPUT_TYPES(cls):
+        cls.size_sizes, cls.size_dict = read_sizes()
+        return {
+            'required': {
+                'size_selected': (cls.size_sizes,), 
+                'multiply_factor': ("INT", "FLOAT")
+            }
         }
 
-    RETURN_TYPES = ("INT","INT")
-    RETURN_NAMES = ("width","height")
+    RETURN_TYPES = ( "INT", "INT")
+    RETURN_NAMES = ( "width", "height")
     FUNCTION = "return_res"
     OUTPUT_NODE = True
     CATEGORY = "Resolution"
 
     def return_res(self, size_selected, multiply_factor):
-        # Extract just the key part for lookup
-        key = size_selected.split(" (")[0]
-        width = self.size_dict[key]["width"] * multiply_factor
-        height = self.size_dict[key]["height"] * multiply_factor
-        return (width, height)
+        # Extract resolution name and dimensions using the key
+        selected_info = self.size_dict[size_selected]
+        width = selected_info["width"] * multiply_factor
+        height = selected_info["height"] * multiply_factor
+        name = selected_info["name"]
+        return (width, height,name)
 
 NODE_CLASS_MAPPINGS = {
     "CascadeResolutions": CascadeResolutions
@@ -40,6 +42,6 @@ def read_sizes():
     file_path = os.path.join(p, 'sizes.json')
     with open(file_path, 'r') as file:
         data = json.load(file)
-    size_sizes = [f"{key} ({value['name']})" for key, value in data['sizes'].items()]
-    size_dict = {key: value for key, value in data['sizes'].items()}
+    size_sizes = [f"{key} - {value['name']}" for key, value in data['sizes'].items()]
+    size_dict = {f"{key} - {value['name']}": value for key, value in data['sizes'].items()}
     return size_sizes, size_dict
