3ca78f2fc284b33a2144c02904382c5d5b0139c1
Abdallah <95714669+al-swaiti@users.noreply.github.com>
2024-02-26 03:31:19 +0300

    Enhance Resolution Selection with Descriptive Names and Multiply Factor in CascadeResolutions (#1)
    * Updated CascadeResolutions to display size with name
    
    * Updated CascadeResolutions to display size with name edited
    
    * Updated CascadeResolutions to display size with name edited
    
    * Updated CascadeResolutions to display size with name edited
    
    * Update README.md
    
    add image preview

--- cascadeResolutions.py
+++ cascadeResolutions.py
@@ -1,41 +1,47 @@
 import os
 import json
+
 class CascadeResolutions:
     def __init__(self):
         pass
     
     @classmethod
-    def INPUT_TYPES(s):
-        s.size_sizes, s.size_dict = read_sizes()
-        return {'required': {'size_selected': (s.size_sizes,)}}
+    def INPUT_TYPES(cls):
+        cls.size_sizes, cls.size_dict = read_sizes()
+        return {
+            'required': {
+                'size_selected': (cls.size_sizes,), 
+                'multiply_factor': ("INT", "FLOAT")
+            }
+        }
 
-    RETURN_TYPES = ("INT","INT")
-    RETURN_NAMES = ("width","height")
+    RETURN_TYPES = ( "INT", "INT")
+    RETURN_NAMES = ( "width", "height")
     FUNCTION = "return_res"
     OUTPUT_NODE = True
     CATEGORY = "Resolution"
 
-    def return_res(self, size_selected):
-        width = self.size_dict[size_selected]["width"]
-        height = self.size_dict[size_selected]["height"]
-        return (width,height)
-        pass
+    def return_res(self, size_selected, multiply_factor):
+        # Extract resolution name and dimensions using the key
+        selected_info = self.size_dict[size_selected]
+        width = selected_info["width"] * multiply_factor
+        height = selected_info["height"] * multiply_factor
+        name = selected_info["name"]
+        return (width, height,name)
 
-# A dictionary that contains all nodes you want to export with their names
-# NOTE: names should be globally unique
 NODE_CLASS_MAPPINGS = {
     "CascadeResolutions": CascadeResolutions
 }
 
-# A dictionary that contains the friendly/humanly readable titles for the nodes
 NODE_DISPLAY_NAME_MAPPINGS = {
     "CascadeResolutions": "Cascade Resolutions"
 }
+
 def read_sizes():
     p = os.path.dirname(os.path.realpath(__file__))
     file_path = os.path.join(p, 'sizes.json')
     with open(file_path, 'r') as file:
         data = json.load(file)
-    size_sizes = list(data['sizes'].keys())
-    size_dict = data['sizes']
+    size_sizes = [f"{key} - {value['name']}" for key, value in data['sizes'].items()]
+    size_dict = {f"{key} - {value['name']}": value for key, value in data['sizes'].items()}
     return size_sizes, size_dict
